649348c708f5d42f482d582af3f77c6b27f2a825
Gopiprasanth Potipireddy <gopiprasanthpotipireddy@Gopiprasanths-MacBook-Pro.local>
2021-12-02 13:29:16 +0530

    reports added

--- RQ-01-Covid-Porportions/analysys.R
+++ RQ-01-Covid-Porportions/analysys.R
@@ -1,16 +1,23 @@
-covid_data<- read.csv(file.choose())
+library(tidyverse)
+covid_data<- read.csv("1_county_level_confirmed_cases.csv")
 colnames(covid_data)
 names(covid_data)
 summary(covid_data)
 
-#Data deaths per 100000 is available for counties so taking the average of counties as the state proportion
-state_death_proportions=aggregate(x=covid_data$deaths_per_100000,by=list(covid_data$state),FUN= mean,na.rm=TRUE, na.action=NULL)
-#Populations n=100000 is same for all the states 
-state_populations<-rep(100000,52)
+' Data is available at county level
+  Creating data at state levelby summation'
 
-prop.test(state_death_proportions$x,state_populations,alternative = "two.sided")
+state_level_deaths=aggregate(list(Deaths=covid_data$deaths,Confirmed=covid_data$confirmed),by=list(State=covid_data$state),FUN= sum,na.rm=TRUE, na.action=NULL)
+state_level_deaths$Survived<-state_level_deaths$Confirmed-state_level_deaths$Deaths
+state_level_deaths$Deaths_Per_100000<- state_level_deaths$Deaths*100000/state_level_deaths$Confirmed
+state_level_deaths$Survived_Per_100000<-state_level_deaths$Survived*100000/state_level_deaths$Confirmed
+state_level_deaths$Deaths_Percentage<- state_level_deaths$Deaths*100/state_level_deaths$Confirmed
+state_level_deaths$Survived_Percentage<-state_level_deaths$Survived*100/state_level_deaths$Confirmed
+
+population<-rep(100000,52)
+
+#Two sided Z-test
+prop.test(state_level_deaths$Deaths_Per_100000,population,p = NULL,alternative = "two.sided",correct = TRUE)
 
 
-#Linear trend of proportions
-prop.trend.test(state_death_proportions$x,state_populations)
 
